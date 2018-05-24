725806fca48567f219032ab54dc609492c9baebd
soochanPark <34018251+bear1704@users.noreply.github.com>
2018-05-24 22:42:41 +0900

    Create client.cpp

---
+++ Network/HW00_/client.cpp
@@ -0,0 +1,82 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <ctime>
+
+#define BUF_SIZE 180
+
+void error_handling();
+
+int main(int argc, char *argv[])
+{
+  int sock;
+  char message[BUF_SIZE];
+  int str_len;
+  int pNumber = 0;
+
+  struct timespec time;
+
+  socklen_t adr_sz;
+
+  struct sockaddr_in serv_adr, from_adr;
+  if(argc != 3)
+  {
+    std::cout << "Usage : " << argv[0] << std::endl;
+    exit(1);
+  }
+
+  sock = socket(PF_INET, SOCK_DGRAM, 0);
+  if(sock == -1)
+    error_handling();
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+
+    while(1)
+    {
+      int sec;
+      int length;
+
+        clock_gettime(CLOCK_MONOTONIC, &time);
+        sec = time.tv_nsec;
+        length = sprintf(message, "%d  %d ",pNumber,sec/100000);
+        pNumber++;
+        std::cout << message << std::endl;
+        //fgets(message, sizeof(message), stdin);
+
+        //if(!strcmp(message,"q\n") || !strcmp(message, "Q\n"))
+            //break;
+
+        sendto(sock, message, strlen(message)+1, 0, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+
+
+
+        //adr_sz = sizeof(from_adr);
+        //str_len = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&from_adr, &adr_sz);
+        //message[str_len] = 0;
+
+        //std::cout << "Message from server   " << message << std::endl;
+        usleep(201000);
+      }
+
+
+      close(sock);
+      return 0;
+
+
+
+    }
+
+void error_handling()
+{
+std::cout << "error_handling" << std::endl;
+  exit(1);
+
+}
